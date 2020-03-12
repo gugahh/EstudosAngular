e6d36a29792bb7bca907b8244c7da5abe39b67dc
gustavo.santos <gustavo.santos@mprj.mp.br>
2020-03-12 10:49:53 -0300

    Ajuste no servidor Http; Criada rota padrão.

--- server.js
+++ server.js
@@ -8,15 +8,20 @@ var app = express();
 
 // Add the abillity to serve our static files from the public directory
 app.use(express.static("public"));
+app.use("/sportstore", express.static("SportStore"));
 
 // Here we serve up our index page
 app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname + "/public/index.html"));
 });
 
+app.get("/sportstore/", function(req, res) {
+  res.sendFile(path.join(__dirname + "/SportStore/SportStoreMain.html"));
+});
+
 var server = app.listen(9000, function() {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log("GadgetEdge.net listening at http://%s:%s", host, port);
+  console.log("Escutando em http://%s:%s", host, port);
 });
